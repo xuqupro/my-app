752cbdabc7ff146db43cad7eb1fbff62ecd8008e
Administrator <admin@example.com>
2019-05-27 22:53:51 +0700

    message

--- vars/template.groovy
+++ vars/template.groovy
@@ -1,4 +1,11 @@
-def call(Map pipelineParams) {
+#!/usr/bin/env groovy
+
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
 
     pipeline {
         agent any
