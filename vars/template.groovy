bdb496b44335b31dedf06f1162d241dec78f4cbf
Administrator <admin@example.com>
2019-05-27 22:43:31 +0700

    message

--- vars/template.groovy
+++ vars/template.groovy
@@ -1,15 +1,37 @@
 #!/usr/bin/env groovy
 
-def call(Map pipelineParams) {
-
-    pipeline {
-        agent any
-        stages {
-            stage('build') {
-                steps {
-                    sh 'mvn clean package -DskipTests=true'
-                }
-            }
-        }
+def call(Map args) {
+  node {
+    stage('Build') {
+       echo "Command: ${args.command}"
     }
+  }
+  return this
+}
+
+def build(Map args) {
+  node {
+    stage('Build') {
+      echo "Command: ${args.command}"
+    }
+  }
+  return this
+}
+
+def unitTest(Map args) {
+  node {
+    stage('Unit Test') {
+      echo "Command: ${args.command}"
+    }
+  }
+  return this
+}
+
+def deploy(Map args) {
+  node {
+    stage('Deploy') {
+      echo "Command: ${args.command}"
+    }
+  }
+  return this
 }
