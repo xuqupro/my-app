88781cc442350b5321123847305ed9074adcbc5c
Administrator <admin@example.com>
2019-05-29 20:10:01 +0700

    message

---
+++ docker/install.sh
@@ -0,0 +1,6 @@
+sudo yum update
+sudo yum install yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
