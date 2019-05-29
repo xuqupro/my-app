57ebf5b17d3d7c4a02fa476573300aebc273a4c5
Administrator <admin@example.com>
2019-05-30 01:17:05 +0700

    message

--- docker/docker-engine.sh
+++ docker/docker-engine.sh
@@ -5,3 +5,10 @@ reboot
 curl -O -sSL https://get.docker.com/rpm/1.7.0/centos-6/RPMS/x86_64/docker-engine-1.7.0-1.el6.x86_64.rpm
 yum localinstall --nogpgcheck docker-engine-1.7.0-1.el6.x86_64.rpm
 service docker start
+
+vim /etc/yum.repos.d/virt7-docker-common-release.repo
+ 
+[virt7-docker-common-release]
+name=virt7-docker-common-release
+baseurl=http://cbs.centos.org/repos/virt7-docker-common-release/x86_64/os/
+gpgcheck=0
